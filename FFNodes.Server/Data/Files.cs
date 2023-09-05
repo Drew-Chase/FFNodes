3575e91bff078d8f932972e747034d809c0eeb50
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 15:05:09 -0400

    added static class to hold file paths

---
+++ FFNodes.Server/Data/Files.cs
@@ -0,0 +1,11 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace FFNodes.Server.Data;
+
+using static Directories;
+
+public static class Files
+{
+    public static string Config { get; } = Path.Combine(Data, "config.json");
+    public static string LatestLog { get; } = Path.Combine(Logs, "latest.log");
+    public static string DebugLog { get; } = Path.Combine(Logs, "debug.log");
+}
