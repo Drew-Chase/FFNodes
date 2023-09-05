0c31c405a6530a6f777c511e6c398004fb79bfb5
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 15:02:04 -0400

    added static class to hold generic static data

---
+++ FFNodes.Server/Data/Data.cs
@@ -0,0 +1,9 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using CLMath;
+
+namespace FFNodes.Server.Data;
+
+public static class Data
+{
+    public static string ConnectionString { get; } = CLAESMath.EncryptStringAES($"{Environment.MachineName}")[..2];
+}
