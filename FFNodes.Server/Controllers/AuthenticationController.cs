aec526e070cffc425501e7c35959df1e7eae80b1
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 15:01:39 -0400

    added authentication contrller

---
+++ FFNodes.Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿/*
+    FFNodes - LFInteractive LLC. 2021-2024
+    FFNodes is a client/server solution for batch processing ffmpeg operations from multiple systems accross the internet.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FFNodes.Server.Controllers;
+
+[Route("/api/auth")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+}
