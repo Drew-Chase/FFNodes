ce69d7a386258b41cc916b7b3bfb2767648255e3
Drew Chase <dcmanproductions@gmail.com>
2023-09-04 21:22:12 -0400

    added processed file model

---
+++ FFNodes.Core/Model/ProcessedFileModel.cs
@@ -0,0 +1,12 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace FFNodes.Server.Model;
+
+public struct ProcessedFileModel
+{
+    public string Name { get; set; }
+    public long OriginalSize { get; set; }
+    public long CompressedSize { get; set; }
+    public bool Successful { get; set; }
+    public TimeSpan Duration { get; set; }
+    public DateTime Completed { get; set; }
+}
