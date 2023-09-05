6ae4bc546fb80ffce48c958286174d4b39df21ca
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 00:45:30 -0400

    added constructor

--- FFNodes.Core/Model/ProcessedFile.cs
+++ FFNodes.Core/Model/ProcessedFile.cs
@@ -4,9 +4,24 @@ namespace FFNodes.Server.Model;
 public struct ProcessedFile
 {
     public string Name { get; set; }
+    public string Path { get; set; }
     public long OriginalSize { get; set; }
     public long CompressedSize { get; set; }
     public bool Successful { get; set; }
     public TimeSpan Duration { get; set; }
     public DateTime Completed { get; set; }
+    public bool HasProcessed { get; set; }
+
+    public ProcessedFile(string path)
+    {
+        FileInfo info = new(path);
+        Name = System.IO.Path.GetFileName(path);
+        Path = path;
+        OriginalSize = info.Length;
+        HasProcessed = false;
+        Completed = DateTime.MinValue;
+        Duration = TimeSpan.Zero;
+        CompressedSize = info.Length;
+        Successful = false;
+    }
 }
