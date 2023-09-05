228f0f38103675dcf6e689a48ceba434a0262cdf
Drew Chase <dcmanproductions@gmail.com>
2023-09-04 21:22:48 -0400

    added boilerplate code for kestrel

---
+++ FFNodes.Server/Application.cs
@@ -0,0 +1,66 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using FFNodes.Server.Data;
+using Serilog;
+using Serilog.Events;
+
+namespace FFNodes.Server
+{
+    public static class Application
+    {
+        private static void Main()
+        {
+            Directory.CreateDirectory("logs");
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(LogEventLevel.Verbose)
+                .WriteTo.File(Path.Combine("logs", "debug.log"), LogEventLevel.Verbose, buffered: true, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 5_000_000)
+                .WriteTo.File(Path.Combine("logs", "latest.log"), LogEventLevel.Information, buffered: true, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 5_000_000)
+                .CreateLogger();
+
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                Log.Information("Stopping Server");
+                Log.CloseAndFlush();
+            };
+
+            Log.Information("Starting FFNodes Server");
+
+            Configuration.Instance.Load();
+
+            Host.CreateDefaultBuilder()
+                .UseSerilog()
+                .ConfigureWebHostDefaults(builder =>
+                {
+                    builder.UseIISIntegration();
+                    builder.UseContentRoot(Directory.GetCurrentDirectory());
+                    builder.UseKestrel(options =>
+                    {
+                        options.ListenAnyIP(Configuration.Instance.Port);
+                    });
+                    builder.UseStartup<Startup>();
+
+                    Log.Information("Server running at {SERVER}", $"http://127.0.0.1:{Configuration.Instance.Port}");
+                }).Build().Run();
+        }
+    }
+
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment evn)
+        {
+            app.UseForwardedHeaders();
+            app.UseMvc();
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+            app.UseSerilogRequestLogging();
+        }
+
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddMvc(action =>
+            {
+                action.EnableEndpointRouting = false;
+            });
+        }
+    }
+}
