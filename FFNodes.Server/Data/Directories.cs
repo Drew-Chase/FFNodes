45882db072e89bcad3d5f1a7934d4232b8a5dd07
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 15:02:16 -0400

    static class to hold directory paths

---
+++ FFNodes.Server/Data/Directories.cs
@@ -0,0 +1,12 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using System.Reflection;
+
+namespace FFNodes.Server.Data;
+
+public static class Directories
+{
+    public static string Root { get; } = Directory.GetParent(Assembly.GetExecutingAssembly().Location)?.FullName ?? Directory.GetCurrentDirectory();
+    public static string Data { get; } = Directory.CreateDirectory(Path.Combine(Root, "Data")).FullName;
+    public static string Users { get; } = Directory.CreateDirectory(Path.Combine(Data, "Users")).FullName;
+    public static string Logs { get; } = Directory.CreateDirectory(Path.Combine(Root, "Logs")).FullName;
+}
