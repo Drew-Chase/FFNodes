b7650453ce9166094be6fe41ec9d604bb1e4e1cb
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 15:05:39 -0400

    added class to handle users

---
+++ FFNodes.Server/Handlers/UserHandler.cs
@@ -0,0 +1,19 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using FFNodes.Core.Model;
+
+namespace FFNodes.Server.Handlers;
+
+public class UserHandler
+{
+    public static readonly UserHandler Instance = Instance ??= new UserHandler();
+    private readonly List<User> users;
+
+    private UserHandler()
+    {
+        users = new();
+    }
+
+    public void Load()
+    {
+    }
+}
