9962ca500252c213188e8b83844c829604aa1615
Drew Chase <dcmanproductions@gmail.com>
2023-09-04 21:22:23 -0400

    added file system controller

---
+++ FFNodes.Server/Controllers/FileSystemController.cs
@@ -0,0 +1,10 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace FFNodes.Server.Controllers;
+
+[Route("/api/fs")]
+[ApiController]
+public class FileSystemController : ControllerBase
+{
+}
