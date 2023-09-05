ea5edcc6fa550ce9cb20eaefb8edd8b78fb8da77
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 00:45:14 -0400

    added user model

---
+++ FFNodes.Core/Model/User.cs
@@ -0,0 +1,14 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using FFNodes.Server.Model;
+
+namespace FFNodes.Core.Model;
+
+public struct User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public TimeSpan ActiveTime { get; set; }
+    public DateTime Join { get; set; }
+    public long Saved { get; set; }
+    public ProcessedFile[] Files { get; set; }
+}
