02fe4d550ecb979f3287d236351266ba542e7c27
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 22:32:41 -0400

    added header

--- FFNodes.Server/Data/Data.cs
+++ FFNodes.Server/Data/Data.cs
@@ -1,4 +1,10 @@
-﻿// LFInteractive LLC. 2021-2024﻿
+﻿/*
+    FFNodes - LFInteractive LLC. 2021-2024
+    FFNodes is a client/server solution for batch processing ffmpeg operations from multiple systems across the internet.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
 using CLMath;
 
 namespace FFNodes.Server.Data;
