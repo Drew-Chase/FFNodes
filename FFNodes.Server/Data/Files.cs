ac6b8b06a70543a3b6c02d350c97bdb32ba0715e
Drew Chase <dcmanproductions@gmail.com>
2023-09-05 22:33:14 -0400

    added header and fixed ambiguous call between namespace and data class

--- FFNodes.Server/Data/Files.cs
+++ FFNodes.Server/Data/Files.cs
@@ -1,11 +1,17 @@
-﻿// LFInteractive LLC. 2021-2024﻿
+﻿/*
+    FFNodes - LFInteractive LLC. 2021-2024
+    FFNodes is a client/server solution for batch processing ffmpeg operations from multiple systems across the internet.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
 namespace FFNodes.Server.Data;
 
 using static Directories;
 
 public static class Files
 {
-    public static string Config { get; } = Path.Combine(Data, "config.json");
+    public static string Config { get; } = Path.Combine(Directories.Data, "config.json");
     public static string LatestLog { get; } = Path.Combine(Logs, "latest.log");
     public static string DebugLog { get; } = Path.Combine(Logs, "debug.log");
 }
