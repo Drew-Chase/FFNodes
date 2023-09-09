e2909767279abe4ed10214b50493f14f60d53cf9
Drew Chase <dcmanproductions@gmail.com>
2023-09-09 17:57:07 -0400

    added FFNetworkClient

---
+++ FFNodes.Client.Core/Networking/FFNetworkClient.cs
@@ -0,0 +1,36 @@
+﻿/*
+    FFNodes - LFInteractive LLC. 2021-2024
+    FFNodes is a client/server solution for batch processing ffmpeg operations from multiple systems across the internet.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using Chase.Networking;
+using FFNodes.Core.Model;
+
+namespace FFNodes.Client.Core.Networking;
+
+public class FFNetworkClient : IDisposable
+{
+    private readonly NetworkClient client;
+
+    public FFNetworkClient(string connectionUrl, Guid userId)
+    {
+        client = new NetworkClient();
+        Uri uri = new(connectionUrl);
+        client.DefaultRequestHeaders.Add("Authentication", uri.LocalPath[1..]);
+        client.DefaultRequestHeaders.Add("User-ID", userId.ToString());
+        client.BaseAddress = new Uri($"http://{uri.Host}:{uri.Port}");
+    }
+
+    public async Task<SystemStatusModel?> GetSystemStatus()
+    {
+        return (await client.GetAsJson($"{client.BaseAddress}api"))?.ToObject<SystemStatusModel>();
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        client.Dispose();
+    }
+}
