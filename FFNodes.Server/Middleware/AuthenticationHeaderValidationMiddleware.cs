2ceb24704130dbd1b21c232a69a44417b6b054c3
Drew Chase <dcmanproductions@gmail.com>
2023-09-09 16:16:55 -0400

    added check if the user's id was provided and passes it through to the controller

--- FFNodes.Server/Middleware/AuthenticationHeaderValidationMiddleware.cs
+++ FFNodes.Server/Middleware/AuthenticationHeaderValidationMiddleware.cs
@@ -5,6 +5,8 @@
     https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
 */
 
+using FFNodes.Core.Model;
+using FFNodes.Server.Handlers;
 using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
 
@@ -21,16 +23,40 @@ public class AuthenticationHeaderValidationMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        if (context.Request.Headers.TryGetValue("code", out StringValues values) && values.Any() && Data.Data.ValidConnection(values[0] ?? ""))
+        context.Items["ConnectedUser"] = null;
+        if (context.Request.Path.Value?.StartsWith("/api") ?? false)
         {
-            await _next(context);
-        }
-        else
-        {
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = 401;
-            await context.Response.WriteAsync(JsonConvert.SerializeObject(new { error = "Invalid or missing connection code." }));
-            return;
+            if (ValidateAuthenticationCode(context))
+            {
+                if (context.Request.Path.Value == "/api/auth/connect" || (context.Request.Path.Value == "/api/auth/user" && context.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase)))
+                {
+                    await _next(context);
+                    return;
+                }
+                if (UserHandler.Instance.GetUserFromHeaders(context, out User user))
+                {
+                    context.Items["ConnectedUser"] = user;
+                    UserHandler.Instance.PingUser(user);
+                    await _next(context);
+                }
+                else
+                {
+                    context.Response.ContentType = "application/json";
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(new { error = "Invalid or missing user id." }));
+                }
+            }
+            else
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(new { error = "Invalid or missing connection code." }));
+            }
         }
     }
+
+    private static bool ValidateAuthenticationCode(HttpContext context) =>
+        context.Request.Headers.TryGetValue("Authentication", out StringValues authentication_code)
+        && authentication_code.Any()
+        && Data.Data.ValidConnection(authentication_code[0] ?? "");
 }
