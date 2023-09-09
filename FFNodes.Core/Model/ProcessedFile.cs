b76d4e34fa316608d789cf1e25227b92fa38455d
Drew Chase <dcmanproductions@gmail.com>
2023-09-09 16:13:31 -0400

    removed redundant filename variable

--- FFNodes.Core/Model/ProcessedFile.cs
+++ FFNodes.Core/Model/ProcessedFile.cs
@@ -9,7 +9,6 @@ namespace FFNodes.Server.Model;
 
 public struct ProcessedFile
 {
-    public string Name { get; set; }
     public string Path { get; set; }
     public long OriginalSize { get; set; }
     public long CompressedSize { get; set; }
@@ -21,7 +20,6 @@ public struct ProcessedFile
     public ProcessedFile(string path)
     {
         FileInfo info = new(path);
-        Name = System.IO.Path.GetFileName(path);
         Path = path;
         OriginalSize = info.Length;
         HasProcessed = false;
