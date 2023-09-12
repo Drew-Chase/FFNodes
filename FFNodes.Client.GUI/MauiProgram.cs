884a69c3879d3dd24dfafc2db200ea8994e36137
Drew Chase <dcmanproductions@gmail.com>
2023-09-12 00:56:54 -0400

    added logger, mutex and crash handler

--- FFNodes.Client.GUI/MauiProgram.cs
+++ FFNodes.Client.GUI/MauiProgram.cs
@@ -5,28 +5,63 @@
     https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
 */
 
+using FFNodes.Client.Core.Handlers;
+using FFNodes.Core;
+using FFNodes.Core.Data;
 using FFNodes.Server.Data;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
-namespace FFNodes.Client.GUI
+namespace FFNodes.Client.GUI;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            MauiAppBuilder builder = MauiApp.CreateBuilder();
-            builder.UseMauiApp<App>();
+        MutexHandler.HandleMutex("FFNodes.Client.GUI");
 
-            builder.Services.AddMauiBlazorWebView();
+        TimeSpan flushTime = TimeSpan.FromSeconds(30);
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Console(LogEventLevel.Verbose)
+            .WriteTo.File(Files.DebugLog, LogEventLevel.Verbose, buffered: true, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 5_000_000, flushToDiskInterval: flushTime)
+            .WriteTo.File(Files.LatestLog, LogEventLevel.Information, buffered: true, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 5_000_000, flushToDiskInterval: flushTime)
+            .WriteTo.File(Files.ErrorLog, LogEventLevel.Error, buffered: true, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 5_000_000, flushToDiskInterval: flushTime)
+            .CreateLogger();
+
+        AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+        {
+            Log.Information("Stopping FFNodes");
+            Log.CloseAndFlush();
+        };
+
+        AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+        {
+            if (e.ExceptionObject is Exception exception)
+            {
+                CrashHandler.HandleCrash(exception);
+            }
+            else
+            {
+                Log.CloseAndFlush();
+            }
+        };
+
+        CommandLineHandler.HandleCommandLine(Environment.GetCommandLineArgs()[1..]); // get the command line arguments, excluding the first argument which is the executable path.
+
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>();
+
+        builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-            builder.Services.AddBlazorWebViewDeveloperTools();
-            builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
-            Configuration.Instance.Load();
+        Configuration.Instance.Load();
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
