b26170067ccf07541e3b6c54bccc4c737bc55d57
Drew Chase <dcmanproductions@gmail.com>
2023-09-13 23:19:50 -0400

    added progress event args

---
+++ FFNodes.Client.Core/Events/FileItemProgressUpdateEventArgs.cs
@@ -0,0 +1,20 @@
+﻿/*
+    FFNodes - LFInteractive LLC. 2021-2024
+    FFNodes is a client/server solution for batch processing ffmpeg operations from multiple systems across the internet.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using FFNodes.Client.GUI.Data;
+
+namespace FFNodes.Client.Core.Events;
+
+public class FileItemProgressUpdateEventArgs : EventArgs
+{
+    public FileItem Item { get; set; }
+
+    public FileItemProgressUpdateEventArgs(FileItem item)
+    {
+        Item = item;
+    }
+}
