b22fc21063e3aaf80201d6b0b891f5145e1078bf
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-11-22 00:45:36 -0500

    Refactor `MediaFile` database interactions:
    - Improve code formatting for better readability in database operation methods.
    - Introduce `does_path_need_probing` method to determine if a file needs re-probing based on last modified timestamp.
    - Enhance SQL query bindings for consistency and reliability.
    - Update `open_pool` function with cleaner options configuration.

--- ffnodes-server/src/media_files/media_file_db.rs
+++ ffnodes-server/src/media_files/media_file_db.rs
@@ -1,13 +1,15 @@
-use sqlx::{ConnectOptions, Executor, SqlitePool, Transaction};
+use crate::media_files::MediaFile;
 use anyhow::Result;
 use log::LevelFilter;
 use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
-use crate::media_files::MediaFile;
+use sqlx::{ConnectOptions, Executor, SqlitePool, Transaction};
+use std::path::Path;
+use std::time::UNIX_EPOCH;
 
 pub async fn initialize() -> Result<()> {
-	let pool = open_pool().await?;
-	pool.execute(
-		r#"CREATE TABLE IF NOT EXISTS `media_files`
+    let pool = open_pool().await?;
+    pool.execute(
+        r#"CREATE TABLE IF NOT EXISTS `media_files`
 (
     path             TEXT    PRIMARY KEY NOT NULL UNIQUE,
     scanned_size     INTEGER NOT NULL,
@@ -21,64 +23,77 @@ pub async fn initialize() -> Result<()> {
     last_modified    INTEGER NOT NULL,
     processed        INTEGER NOT NULL DEFAULT 0
 )"#,
-	)
-	    .await?;
+    )
+    .await?;
 
-	Ok(())
+    Ok(())
 }
 
-
-impl MediaFile{
-	pub async fn insert(&self, transaction: &mut Transaction<'_,sqlx::sqlite::Sqlite>) -> Result<()> {
-		sqlx::query(
-			r#"INSERT INTO media_files
+impl MediaFile {
+    pub async fn insert(
+        &self,
+        transaction: &mut Transaction<'_, sqlx::sqlite::Sqlite>,
+    ) -> Result<()> {
+        sqlx::query(
+            r#"INSERT INTO media_files
 			(path, scanned_size, scanned_bit_rate, duration, width, height, frames, last_modified, processed)
-			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"#
-		)
-			.bind(self.path.to_string_lossy().to_string())
-			.bind(self.scanned_size as i64)
-			.bind(self.scanned_bit_rate as i64)
-			.bind(self.duration as f64)
-			.bind(self.width as i64)
-			.bind(self.height as i64)
-			.bind(self.frames as i64)
-			.bind(self.last_modified as i64)
-			.bind(self.processed)
-			.execute(&mut **transaction)
-			.await?;
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"#,
+        )
+        .bind(self.path.to_string_lossy().to_string())
+        .bind(self.scanned_size as i64)
+        .bind(self.scanned_bit_rate as i64)
+        .bind(self.duration as f64)
+        .bind(self.width as i64)
+        .bind(self.height as i64)
+        .bind(self.frames as i64)
+        .bind(self.last_modified as i64)
+        .bind(self.processed)
+        .execute(&mut **transaction)
+        .await?;
 
-		Ok(())
-	}
+        Ok(())
+    }
 
-	pub async fn insert_direct(&self, pool: &SqlitePool) -> Result<()> {
-		sqlx::query(
-			r#"INSERT INTO media_files
+    pub async fn insert_direct(&self, pool: &SqlitePool) -> Result<()> {
+        sqlx::query(
+            r#"INSERT INTO media_files
 			(path, scanned_size, scanned_bit_rate, duration, width, height, frames, last_modified, processed)
-			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"#
-		)
-			.bind(self.path.to_string_lossy().to_string())
-			.bind(self.scanned_size as i64)
-			.bind(self.scanned_bit_rate as i64)
-			.bind(self.duration as f64)
-			.bind(self.width as i64)
-			.bind(self.height as i64)
-			.bind(self.frames as i64)
-			.bind(self.last_modified as i64)
-			.bind(self.processed)
-			.execute(pool)
-			.await?;
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"#,
+        )
+        .bind(self.path.to_string_lossy().to_string())
+        .bind(self.scanned_size as i64)
+        .bind(self.scanned_bit_rate as i64)
+        .bind(self.duration as f64)
+        .bind(self.width as i64)
+        .bind(self.height as i64)
+        .bind(self.frames as i64)
+        .bind(self.last_modified as i64)
+        .bind(self.processed)
+        .execute(pool)
+        .await?;
 
-		Ok(())
-	}
+        Ok(())
+    }
+
+    pub async fn does_path_need_probing(path: impl AsRef<Path>, pool: &SqlitePool) -> Result<bool> {
+        let path = path.as_ref();
+        let last_modified = path.metadata()?.modified()?;
+        let db_last_modified =
+            sqlx::query_scalar::<_, i64>(r#"SELECT last_modified FROM media_files WHERE path = ?"#)
+                .bind(path.to_string_lossy().to_string().clone())
+                .fetch_optional(pool)
+                .await?
+                .unwrap_or(0);
+        Ok(last_modified.duration_since(UNIX_EPOCH)?.as_secs() > (db_last_modified as u64))
+    }
 }
-
 
 pub async fn open_pool() -> Result<SqlitePool> {
-	let options = SqliteConnectOptions::new()
-		.journal_mode(SqliteJournalMode::Wal)
-		.foreign_keys(true)
-		.filename("app.db")
-		.log_statements(LevelFilter::Trace)
-		.create_if_missing(true);
-	Ok(SqlitePool::connect_with(options).await?)
+    let options = SqliteConnectOptions::new()
+        .journal_mode(SqliteJournalMode::Wal)
+        .foreign_keys(true)
+        .filename("app.db")
+        .log_statements(LevelFilter::Trace)
+        .create_if_missing(true);
+    Ok(SqlitePool::connect_with(options).await?)
 }
